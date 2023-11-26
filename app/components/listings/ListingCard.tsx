30ef1f0c896fc30125257d30c0374782813d51a8
Egli <eglimiraka7@gmail.com>
2023-11-26 15:18:34 +0100

    Favorites

--- app/components/listings/ListingCard.tsx
+++ app/components/listings/ListingCard.tsx
@@ -4,6 +4,10 @@ import { SafeUser } from "@/app/types";
 import { Listing, Reservation } from "@prisma/client";
 import { useRouter } from "next/navigation";
 import React, { useCallback, useMemo } from "react";
+import { format } from "date-fns";
+import Image from "next/image";
+import HeartButton from "../HeartButton";
+import Button from "../Button";
 
 interface ListingCardProps {
   data: Listing;
@@ -43,23 +47,81 @@ const ListingCard: React.FC<ListingCardProps> = ({
   );
 
   const price = useMemo(() => {
-    if(reservation) {
-        return reservation.totalPrice
+    if (reservation) {
+      return reservation.totalPrice;
     }
-    return data.price
-  }
-  , [data.price, reservation])
+    return data.price;
+  }, [data.price, reservation]);
 
-  const reservationDate=useMemo(() => {
-    if(!reservation) {
-        return null
+  const reservationDate = useMemo(() => {
+    if (!reservation) {
+      return null;
     }
 
-    const start = new Date(reservation.startDate)
-    const end = new Date(reservation.endDate)
-  }, [])
+    const start = new Date(reservation.startDate);
+    const end = new Date(reservation.endDate);
 
-  return <div>ListingCard</div>;
+    return `${format(start, "PP")} - ${format(end, "PP")}`;
+  }, [reservation]);
+
+  return (
+    <div className='col-span-1 cursor-pointer group'>
+      <div className='flex flex-col gap-2 w-full'>
+        <div
+          className='
+          aspect-square 
+          w-full 
+          relative 
+          overflow-hidden 
+          rounded-xl
+        '
+        >
+          <Image
+            fill
+            onClick={() => router.push(`/listings/${data.id}`)}
+            className='
+            object-cover 
+            h-full 
+            w-full 
+            group-hover:scale-110 
+            transition
+          '
+            sizes='(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw'
+            priority={true}
+            src={data.imageSrc}
+            alt='Listing'
+          />
+          <div
+            className='
+          absolute
+          top-3
+          right-3
+        '
+          >
+            <HeartButton listingId={data.id} currentUser={currentUser} />
+          </div>
+        </div>
+        <div className='font-semibold text-lg'>
+          {location?.region}, {location?.label}
+        </div>
+        <div className='font-light text-neutral-500'>
+          {reservationDate || data.category}
+        </div>
+        <div className='flex flex-row items-center gap-1'>
+          <div className='font-semibold'>$ {price}</div>
+          {!reservation && <div className='font-light'>night</div>}
+        </div>
+        {onAction && actionLabel && (
+          <Button
+            disabled={disabled}
+            small
+            label={actionLabel}
+            onClick={handleCancel}
+          />
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default ListingCard;
